09b1609e77f7491323f9066692cea90d7e481731
Jasper Beaurain <info@jasperbeaurain.com>
2020-11-16 19:22:13 +0100

    remove not needed state keeping in datepicker

--- trm/src/components/Datepicker/Datepicker.tsx
+++ trm/src/components/Datepicker/Datepicker.tsx
@@ -2,27 +2,12 @@ import React from "react";
 import { parse } from "date-fns";
 
 interface DatepickerProps {
-    value?: Date,
     onChange: (date: Date) => void,
 }
 
-interface DatepickerState {
-    value?: Date,
-}
-
-class Datepicker extends React.Component<DatepickerProps, DatepickerState> {
-    constructor(props: DatepickerProps) {
-        super(props);
-        this.state = {};
-    }
-
+class Datepicker extends React.Component<DatepickerProps> {
     updateValue = (dateString: string) => {
         const dateValue = parse(dateString, "yyyy-MM-dd", new Date());
-
-        this.setState({
-            value: dateValue,
-        });
-
         this.props.onChange(dateValue);
     }
 
