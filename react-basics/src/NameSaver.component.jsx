ea0375c7f995c4724f34384700ac267134436ff9
Jasper Beaurain <info@jasperbeaurain.com>
2020-11-17 12:13:48 +0100

    conditionsals

--- react-basics/src/NameSaver.component.jsx
+++ react-basics/src/NameSaver.component.jsx
@@ -2,10 +2,40 @@ import React from "react";
 
 class NameSaver extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+        };
+    }
+
     render() {
+
+        const greeting = this.state.name.length > 0
+            ? <p>name: { this.state.name }</p>
+            : <p>Please fill in your name</p>;
+
         return (
             <div>
                 <h3>Name saver</h3>
+                { greeting }
+                <input
+                    type="text"
+                    value={this.state.name}
+                    onChange={(event) => {
+                        this.setState({
+                            name: event.target.value,
+                        });
+                    }}
+                />
+                <button
+                    onClick={() => {
+                        this.setState({
+                            name: "reset",
+                        });
+                    }}
+                >change</button>
             </div>
         );
     }
